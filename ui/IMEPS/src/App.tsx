0b681e9124e599092873350796ba61ee506437ff
hannaaad <hannaaad15@gmail.com>
2025-02-15 20:40:39 +0200

    %dmin+uni

--- ui/IMEPS/src/App.tsx
+++ ui/IMEPS/src/App.tsx
@@ -1,21 +1,20 @@
-import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar.tsx";
 import Home from "./pages/Home.tsx";
 import {Box} from "@chakra-ui/react";
 import LoginPage from "./pages/LoginPage.tsx";
-import University from "./pages/University.tsx";
-import Adminpage from "./pages/Admin.tsx";
+import UniversityPage from "./pages/UniversityPage.tsx";
+import Adminpage from "./pages/AdminPage.tsx";
 
 const App = () => {
     return (
         <Router>
-            <Navbar width={"full"}/>
+            <Navbar/>
             <Box backgroundColor={"white"} height={"100vh"} width={"100vw"} overflow={"hidden"}>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path={"/login"} element={<LoginPage/>}/>
-                    {<Route path="/universities" element={<University/>}/>}
+                    {<Route path="/universities" element={<UniversityPage/>}/>}
                     {<Route path="/admin" element={<Adminpage/>}/>}
                     {/*<Route path="/bourses" element={<Bourse/>}/>*/}
                 </Routes>
